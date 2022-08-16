37d2308ab90f7af6e37de04449d01b8880b7a836
Bernardo010 <110498945+Bernardo010@users.noreply.github.com>
2022-08-16 19:48:28 -0300

    Add files via upload

---
+++ 006-projeto/app.js
@@ -0,0 +1,10 @@
+var num1 = 10; 
+var num2 = 15;
+var num3 = 5;
+if ( num1 > num2 ) {
+console.log(`O número ${num1} é maior que o ${num2}`)
+} else {
+    console.log(`O número ${num1} é menor que o número ${num2}`)
+}
+
+
