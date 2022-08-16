6250cdf68329828a6e0d575d2a46f3e6c64ea131
Bernardo010 <110498945+Bernardo010@users.noreply.github.com>
2022-08-16 20:29:17 -0300

    Update app.js

--- 006-projeto/app.js
+++ 006-projeto/app.js
@@ -6,5 +6,11 @@ console.log(`O número ${num1} é maior que o ${num2}`)
 } else {
     console.log(`O número ${num1} é menor que o número ${num2}`)
 }
-
+console.log(num3 > num1)
+console.log(num2 > num1)
+if ( num3 > num1 || num2 > num1) {
+    console.log(`1 - ${num1} ${num2} ${num3}`)
+} else {
+    console.log(`2 - ${num1} ${num2} ${num3} `)
+}
 
