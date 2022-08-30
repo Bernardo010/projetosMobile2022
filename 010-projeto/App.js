f5e07696bd5737dc98e751c9ec9e2467dd6591b2
Bernardo010 <110498945+Bernardo010@users.noreply.github.com>
2022-08-29 21:15:16 -0300

    Add files via upload

--- 010-projeto/App.js
+++ 010-projeto/App.js
@@ -1,39 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,Button ,Text, View ,alert,SafeAreaView } from 'react-native';
-import constants from 'expo-constants';
+//import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import Constants from 'expo-constants';
 
-function separetor () {
-  return <view style={style separator} />
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+export default function App() {
+  return (
+   <SafeAreaView style={styles.container} >
+    <View>
+      <Text style={styles.title}>
+        O título e o manipulador onPress são obrigatórios. Recomenda-se definir acessibilidadeLabel para ajudar a tornar seu aplicativo utilizável por todos.      
+      </Text>
+      <Button        title="Aperte"        onPress={() => alert('Botão Simples')}
+      />
+    </View>
+    <Separator />
+    <View>
+      <Text style={styles.title}>
+      Ajuste a cor de uma maneira que pareça padrão em cada plataforma. No iOS, o suporte de cor controla a cor do texto. No Android, a cor ajusta a cor de fundo do botão.
+      </Text>
+      <Button
+        title="Aperte"        color="#f194ff"        onPress={() => alert('Botão com cor ajustada pressionado')}      />
+    </View>
+    <Separator />
+    <View>
+      <Text style={styles.title}>
+      Todas as interações para o componente estão desabilitadas.
+      </Text>
+      <Button        title="Aperte"        disabled        onPress={() => alert('Nao pode ser pressionado')}      />
+    </View>
+    <Separator />
+    <View>
+      <Text style={styles.title}>
+        Essa estratégia de layout permite que o título defina a largura do botão.
+      </Text>
+      <View style={styles.fixToText}>
+        <Button          title="Botão a esquerda"          onPress={() => alert('Botão a esquerda')}        />
+        <Button          title="Botão a direita"          onPress={() => alert('Botão a direita')}        />
+      </View>
+    </View>
+  </SafeAreaView>
+  );
 }
 
-
-export default function app() {
-  return (
-    <SafeAreaView style={StyleSheet.container} ><view>
-      <Button title="aperte aqui" onPress={()  => {alert ('simple')
-     ) }}/>
-    </view>
-    <sparator/>
-    <view>
-      <text style={style.title}>
-     adjust the color in a way that looks standard on each plataform. On IOS,the color prop controld the color of the text. On Android, the color adjusts the background color of the button .
-     </text>
-     <Button 
-     title="aperte aqui"
-     color="#f194ff"
-     onPress={() => {alert('Button with adjusted color Pressed')}}
-     />
-     </view>
-     <separetor />
-      </view>  
-       <Text style={StyleSheet.title}>
-this layout strategy lets the title define de width of the Button.</Text>
-<view style={styles.fixtotext}>
-<Button
-title="botão esquerdo"
-OnPress={() => {alert('left Button pressed ')}}
-/>
-<Button
-title="botão direto "
-OnPress={() => {alert('right button pressed')}}
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },  
+});
